3848a35466f8b10fc346aca984449b2bc6edb4cc
velozdev <anthony.delorie@velozis.com>
2025-07-21 17:22:29 +0000

    Move components to src/components and clean up old files

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode, createElement, CSSProperties } from "react";
+import { Component, ReactNode, createElement, CSSProperties } from 'react';
 
 export interface InputProps {
     content: string;
@@ -6,17 +6,18 @@ export interface InputProps {
 
 export class Badge extends Component<InputProps> {
     render(): ReactNode {
-
         let len = this.props.content.length;
         len = len === 1 ? 2 : len;
         const rt = (len - 1) * -0.75;
         const customStyle: CSSProperties = {
             right: rt + 'em',
-            width: len + 'em'
-        }
+            width: len + 'em',
+        };
 
         return (
-                <div className="sr-icon-badge-badge" style={customStyle}>{this.props.content}</div>
+            <div className="sr-icon-badge-badge" style={customStyle}>
+                {this.props.content}
+            </div>
         );
     }
 }
--- src/components/StyleIconBadge.tsx
+++ src/components/StyleIconBadge.tsx
@@ -1,32 +1,32 @@
-import { Component, ReactNode, createElement, CSSProperties } from "react";
+import { Component, ReactNode, createElement, CSSProperties } from 'react';
 
-import { Badge } from "./Badge";
+import { Badge } from './Badge';
 
 export interface InputProps {
     class: string;
     style?: CSSProperties;
     badgeContent: string;
-    characters: string;
+    iconStyle: string;
 }
 
-export class CharacterIconBadge extends Component<InputProps> {
+export class StyleIconBadge extends Component<InputProps> {
     render(): ReactNode {
-        const customClasses = "sr-icon-badge " + this.props.class;
-        
+        const customClasses = 'sr-icon-badge ' + this.props.class;
+
         let len = this.props.badgeContent.length;
         len = len === 1 ? 2 : len;
-        const rt = (((len - 1) * 0.75) * 0.6) + 2;
+        const rt = (len - 1) * 0.75 * 0.6 + 2;
         const mt = 1.1 * 0.6;
         const customStyle: CSSProperties = {
             marginTop: mt + 'em',
-            width: 'calc(' + rt + 'em + 2px)'
-        }
-        
+            width: 'calc(' + rt + 'em + 2px)',
+        };
+
         return (
             <div className={customClasses} style={this.props.style}>
                 <div className="sr-icon-badge-container" style={customStyle}>
                     <div className="sr-icon-badge-content">
-                        {this.props.characters}
+                        <div className={this.props.iconStyle}></div>
                         <Badge content={this.props.badgeContent} />
                     </div>
                 </div>
