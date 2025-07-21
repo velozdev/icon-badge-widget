dd28c168dd1f725321cc5f8eed58307229a7d32c
velozdev <anthony.delorie@velozis.com>
2025-07-21 11:13:26 -0400

    Create Badge.tsx

---
+++ src/ui/components/Badge.tsx
@@ -0,0 +1,22 @@
+import { Component, ReactNode, createElement, CSSProperties } from "react";
+
+export interface InputProps {
+    content: string;
+}
+
+export class Badge extends Component<InputProps> {
+    render(): ReactNode {
+
+        let len = this.props.content.length;
+        len = len === 1 ? 2 : len;
+        const rt = (len - 1) * -0.75;
+        const customStyle: CSSProperties = {
+            right: rt + 'em',
+            width: len + 'em'
+        }
+
+        return (
+                <div className="sr-icon-badge-badge" style={customStyle}>{this.props.content}</div>
+        );
+    }
+}
